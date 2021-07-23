43391b722bff726a70924ce38cfaaa7a752b659a
kingbaddo <wizzyblaq94@gmail.com>
2021-07-23 23:49:49 +0100

    title section

---
+++ src/comps/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>Gallegram</h1>
+      <h2>Your Pictures</h2>
+      <p>How about saving your favourite pictures here.</p>
+    </div>
+  )
+}
+
+export default Title;
