bcc2799ec6e3f2bb931dbde90048b714ab0f1ad0
kingbaddo <wizzyblaq94@gmail.com>
2021-07-24 09:45:29 +0100

    sample data if no internect connection

---
+++ src/comps/Sample.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Sample = () => {
+    return (
+        <div className="sample-grid">
+            <div className="sample-container">
+            <h3>No internet Connection</h3>
+            <h3>No internet Connection</h3>
+            <h3>No internet Connection</h3>
+            <h3>No internet Connection</h3>
+            <h3>No internet Connection</h3>
+            <h3>No internet Connection</h3>
+            </div>
+        </div>
+    )
+}
+
+export default Sample
