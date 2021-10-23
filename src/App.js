8f2b58d3e83e24f2ebe1e0a2f088c20c194ab0e9
kingbaddo <wizzyblaq94@gmail.com>
2021-10-23 12:10:54 +0100

    the app container

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Count from './comps/Count';
 import ImageGrid from './comps/ImageGrid';
+import Modal from './comps/Modal';
 import Title from './comps/Title';
 import UploadForm from './comps/uploadForm';
 //mport SidePost from './comps/SidePost';
 
 function App() {
+  const [ selectedimg, setSelectedimg ] = useState(null);
   return (
     <div className="App">
       <Title/>
       <UploadForm />
-      <ImageGrid />
-     
+      <ImageGrid setSelectedimg={setSelectedimg}/>
+      {selectedimg && <Modal selectedimg={selectedimg} setSelectedimg={setSelectedimg}/> }
+
+     <Count />
     </div>
   );
 }
